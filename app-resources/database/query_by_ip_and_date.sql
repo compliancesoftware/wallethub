4235fcab7215eef5dc769a0f150adb597ad841e6
Douglas Fernandes <br.com.age@gmail.com>
2018-01-04 17:54:22 -0300

    [WalletHub Parser Application v1.0] - Douglas Fernandes: Fix the SQL query to consult blocked IP's inside a period and above a determined quantity of requests;

--- app-resources/database/query_by_ip_and_date.sql
+++ app-resources/database/query_by_ip_and_date.sql
@@ -1,2 +1,2 @@
 use wallethub;
-SELECT * FROM blocked_ips WHERE ip = '192.168.234.82' and date >= '2017-01-01 00:00:00' and date <= '2017-01-01 00:30:00';
+SELECT ip, count(id) FROM blocked_ips WHERE date >= '2017-01-01 00:00:00' and date <= '2017-01-01 15:30:00' GROUP BY ip HAVING COUNT(id) > 100;
